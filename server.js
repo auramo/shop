f4d4652bf607e3358ecba67f05052bc250d31da1
Perttu Auramo <perttu.auramo@gmail.com>
2015-12-20 00:13:50 +0200

    Over-engineer the search-stuff

--- server.js
+++ server.js
@@ -8,16 +8,22 @@ app.use(express.static('public'))
 var productListTemplate = 'productListTemplate.handlebars'
 var productDataFile = 'productData.json'
 
-var catalog = JSON.parse(fs.readFileSync(productDataFile))
+var plainCatalog = JSON.parse(fs.readFileSync(productDataFile))
+var catalogWithSearchData = {products: plainCatalog.products.map(addSearchData)}
 
 var template = Handlebars.compile(fs.readFileSync(productListTemplate).toString());
 
 app.get('/', function (req, res) {
-    res.send(template(catalog))
-});
+    res.send(template(catalogWithSearchData))
+})
 
 var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
     console.log('Shop listening at http://%s:%s', host, port)
-});
+})
+
+function addSearchData(product) {
+    product.searchData = product.name.toLowerCase() // we could add other stuff like tags here as well
+    return product
+}
