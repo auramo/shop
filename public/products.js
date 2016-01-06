0a8ef896f3b1a0dbb440336594ac8b7b76c7cd17
Perttu Auramo <perttu.auramo@gmail.com>
2016-01-06 12:42:08 +0200

    This branch demonstrates rendering the product data client-side
    Product data is fetched via Ajax
    Server-side doesn't do anything any more, only serves static files
    The HTML is no longer in handlebars-template, it's just a static index.html under /public
    Lodash is added for client-side mapping of data (could've been done with standard mArray ap function as well probably)
    handlebars depdencency is removed

--- public/products.js
+++ public/products.js
@@ -1,11 +1,43 @@
-$(function() {
-    $('#search').throttledInput(200, function(val) {
-        if (val.length === 0) {
-            $( ".product-row").show()
-            return
-        } else {
-            $( ".product-row").hide()
-        }
-        $( '.product-name[data-name*="'+val.toLowerCase()+'"]').parent().show()
+(function() {
+    function initFilter() {
+        $('#search').throttledInput(200, function(val) {
+            if (val.length === 0) {
+                $( ".product-row").show()
+                return
+            } else {
+                $( ".product-row").hide()
+            }
+            $( '.product-name[data-name*="'+val.toLowerCase()+'"]').parent().show()
+        })
+    }
+
+    function fetchProducts() {
+        $.get( "/productData.json", function(productData) {
+            var tableRows = _.map(
+                productData.products,
+                function(product) {
+                    return '<tr class="product-row">' +
+                        '<td class="product-name" data-name="' +
+                        product.name.toLowerCase() +
+                        '">' +
+                        product.name +
+                        '</td>' +
+                        '<td>' +
+                        product.price +
+                        '</td>' +
+                        '<td>' +
+                        product.availability +
+                        '</td>' +
+                        '</tr>'
+                })
+            $('.product-table-body').html(tableRows.join(""))
+        }).fail(function(req, statusStr) {
+            alert("Fetching products failed " + statusStr)
+        })
+    }
+
+    $(function() {
+        fetchProducts()
+        initFilter()
     })
-})
+})();
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require('express')
-var Handlebars = require('handlebars')
 var fs = require('fs')
 
 var app = express()
 app.use(express.static('public'))
-
-var productListTemplate = 'productListTemplate.handlebars'
-var productDataFile = 'productData.json'
 
 app.get('/', function (req, res) {
     res.send(catalogPage())
@@ -17,15 +13,3 @@ var server = app.listen(8080, function () {
     var port = server.address().port
     console.log('Shop listening at http://%s:%s', host, port)
 })
-
-function catalogPage() {
-    var plainCatalog = JSON.parse(fs.readFileSync(productDataFile))
-    var catalogWithSearchData = {products: plainCatalog.products.map(addSearchData)}
-    var template = Handlebars.compile(fs.readFileSync(productListTemplate).toString())
-    return template(catalogWithSearchData)
-}
-
-function addSearchData(product) {
-    product.searchData = product.name.toLowerCase() // we could add other stuff like tags here as well
-    return product
-}
